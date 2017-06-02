af3a20cf9bb8f04a6a9cad65ca716b0ecb5eba8a
ganjinlei <404032765@qq.com>
2017-06-02 11:28:08 +0800

    Signed-off-by: ganjinlei <404032765@qq.com>

---
+++ test/application/admin/controller/Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by sunny.
+ * User: sunny
+ * For Darling
+ * Date: 2016/9/30
+ * Time: 14:29
+ */
+namespace app\admin\controller;
+
+class Test extends Base{
+    public function index(){
+        return "Hello Tp5";
+    }
+
+    public function hello($id){
+        echo $id;
+    }
+}
